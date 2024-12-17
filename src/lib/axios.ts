2f85da915a9d5a8e898c5e7b5ba1fe077eb7f41d
LEGENDO0991 <97464074+LEGENDO0991@users.noreply.github.com>
2024-12-17 23:43:42 +0000

    added production url

--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export default axios.create({
-  baseURL: import.meta.env.VITE_BASE_API_URL || "http://localhost:3000/api/",
+  baseURL: import.meta.env.PROD ? "https://pulse-backend-526d.onrender.com/api/" : "http://localhost:3000/api/",
   headers:{
     Authorization:localStorage.getItem("token")
   }
